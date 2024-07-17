e540c75be7a1bff12dc733932e843be2043ec9e9
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-07-17 13:33:31 +0100

    adicionado a outra modal e utilização do x para fechar modal

--- src/app.tsx
+++ src/app.tsx
@@ -4,16 +4,26 @@ import {
   ArrowRight,
   UserRoundPlus,
   Settings2,
+  X,
 } from "lucide-react";
 import { useState } from "react";
 
 export function App() {
   const [isGuestsInputOpen, setsGuestsInputOpen] = useState<boolean>(false);
+  //para chamar outro modal como tela
+  const [isGuestsModalOpen, setisGuestsModalOpen] = useState<boolean>(false);
 
+  //para chamar o modal de inputs
   function OpenIsGuestInput() {
     setsGuestsInputOpen(true);
   }
+  function CloseIsGuestInput() {
+    setsGuestsInputOpen(false);
+  }
 
+  function OpenIsGuestModal() {
+    setisGuestsModalOpen(true);
+  }
   return (
     <>
       <div className="h-screen flex items-center justify-center">
@@ -30,6 +40,7 @@ export function App() {
               <div className="flex items-center gap-2  flex-1">
                 <MapPin className="size-5 text-zinc-400" />
                 <input
+                  disabled={isGuestsInputOpen}
                   type="text"
                   placeholder="Para onde você vai"
                   className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
@@ -38,6 +49,7 @@ export function App() {
               <div className="flex items-center gap-2">
                 <Calendar className="size-5 text-zinc-400" />
                 <input
+                  disabled={isGuestsInputOpen}
                   type="text"
                   placeholder="Quando?"
                   className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
@@ -46,7 +58,10 @@ export function App() {
 
               <div className="w-px h-6 bg-zinc-800" />
               {isGuestsInputOpen ? (
-                <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                <button
+                  className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+                  onClick={CloseIsGuestInput}
+                >
                   alterar local/data
                   <Settings2 className="size-5 text-zinc-200" />
                 </button>
@@ -64,14 +79,16 @@ export function App() {
             {
               isGuestsInputOpen && (
                 <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
-                  <div className="flex items-center gap-2  flex-1">
+                  <button
+                    type="button"
+                    onClick={OpenIsGuestModal}
+                    className="flex items-center gap-2  flex-1"
+                  >
                     <UserRoundPlus className="size-5 text-zinc-400" />
-                    <input
-                      type="text"
-                      placeholder="Quem estará na viagem?"
-                      className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                    />
-                  </div>
+                    <span className="text-zinc-400 text-lg text-left">
+                      Quem estará na viagem?
+                    </span>
+                  </button>
 
                   <div className="w-px h-6 bg-zinc-800"></div>
                   <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
@@ -98,6 +115,17 @@ export function App() {
             </a>
           </p>
         </div>
+        {isGuestsModalOpen && (
+          // bg-black/60 => isto é uma opacidade
+          <div className="fixed inset-0 bg-black/70 flex items-center justify-center">
+            <div className="w-[640px] rounded-xl py-5 px-6  bg-zinc-900">
+              <div className="flex items-center justify-between">
+                <h2>Selececionar convidadados</h2>
+                <X className="size-5 text-zinc-400" />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
