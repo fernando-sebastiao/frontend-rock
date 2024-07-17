acc8be288702d628cf3ce62311dac04a6249b326
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-07-17 14:40:33 +0100

    terminado de fazer a modal de convidar amigo

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import {
   UserRoundPlus,
   Settings2,
   X,
+  AtSign,
 } from "lucide-react";
 import { useState } from "react";
 
@@ -23,6 +24,9 @@ export function App() {
 
   function OpenIsGuestModal() {
     setisGuestsModalOpen(true);
+  }
+  function CloseIsGuestModal() {
+    setisGuestsModalOpen(false);
   }
   return (
     <>
@@ -118,11 +122,60 @@ export function App() {
         {isGuestsModalOpen && (
           // bg-black/60 => isto é uma opacidade
           <div className="fixed inset-0 bg-black/70 flex items-center justify-center">
-            <div className="w-[640px] rounded-xl py-5 px-6  bg-zinc-900">
-              <div className="flex items-center justify-between">
-                <h2>Selececionar convidadados</h2>
-                <X className="size-5 text-zinc-400" />
+            <div className="w-[640px] rounded-xl py-5 px-6  bg-zinc-900 space-y-5">
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <h2 className="text-lg font-semibold">
+                    Selecionar convidadados
+                  </h2>
+                  <button type="button" onClick={CloseIsGuestModal}>
+                    {" "}
+                    <X className="size-5 text-zinc-400" />
+                  </button>
+                </div>
+                <p className="text-sm text-zinc-400">
+                  Os convidados irão receber e-mails para confirmar a
+                  participação nas viagens!
+                </p>
               </div>
+
+              <div className="flex flex-wrap gap-2">
+                <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                  <span className="text-zinc-300">jessica.white@gmail.com</span>
+                  <button type="button">
+                    <X className="size-4 text-zinc-400" />
+                  </button>
+                </div>
+                <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                  <span className="text-zinc-300">jessica.white@gmail.com</span>
+                  <button type="button">
+                    <X className="size-4 text-zinc-400" />
+                  </button>
+                </div>
+                <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                  <span className="text-zinc-300">jessica.white@gmail.com</span>
+                  <button type="button">
+                    <X className="size-4 text-zinc-400" />
+                  </button>
+                </div>
+              </div>
+
+              <div className="w-full h-px bg-zinc-800" />
+
+              <form className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <div className="px-2 flex items-center flex-1 gap-2">
+                  <AtSign className="text-zinc-400 size-4" />
+                  <input
+                    type="text"
+                    placeholder="Digite o e-mail do convidado"
+                    className="bg-transparent text-sm placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+                <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                  Convidar amigos(as)
+                  <ArrowRight className="size-5 text-lime-950" />
+                </button>
+              </form>
             </div>
           </div>
         )}
