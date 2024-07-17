ba91f64c917a626e4591287d8987aaad3a144c79
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-07-17 12:28:37 +0100

    adicionado o segundo modal de pergunta a quem vai participar

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,13 @@
 import { MapPin, Calendar, ArrowRight, UserRoundPlus } from "lucide-react";
+import { useState } from "react";
 
 export function App() {
+  const [isGuestsInputOpen, setsGuestsInputOpen] = useState(false);
+
+  function OpenIsGuestInput() {
+    setsGuestsInputOpen(true);
+  }
+
   return (
     <>
       <div className="h-screen flex items-center justify-center">
@@ -12,29 +19,61 @@ export function App() {
               Convide seus amigos e planeja a sua próxima viagem
             </p>
           </div>
+          <div className="space-y-4">
+            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
+              <div className="flex items-center gap-2  flex-1">
+                <MapPin className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Para onde você vai"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <Calendar className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Quando?"
+                  className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
+                />
+              </div>
 
-          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
-            <div className="flex items-center gap-2  flex-1">
-              <MapPin className="size-5 text-zinc-400" />
-              <input
-                type="text"
-                placeholder="Para onde você vai"
-                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-              />
+              <div className="w-px h-6 bg-zinc-800"></div>
+              <button
+                onClick={OpenIsGuestInput}
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+              >
+                Continuar
+                <ArrowRight className="size-5 text-lime-950" />
+              </button>
             </div>
-            <div className="flex items-center gap-2">
-              <Calendar className="size-5 text-zinc-400" />
-              <input
-                type="text"
-                placeholder="Quando?"
-                className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
-              />
-            </div>
-            <div className="w-px h-6 bg-zinc-800"></div>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-              Continuar
-              <ArrowRight className="size-5 text-lime-950" />
-            </button>
+            {/* //aqui pode se colocar o ponto de interrogação ? também  */}
+            {
+              isGuestsInputOpen && (
+                <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
+                  <div className="flex items-center gap-2  flex-1">
+                    <MapPin className="size-5 text-zinc-400" />
+                    <input
+                      type="text"
+                      placeholder="Quem estará na viagem?"
+                      className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                    />
+                  </div>
+
+                  <div className="w-px h-6 bg-zinc-800"></div>
+                  <button
+                    onClick={OpenIsGuestInput}
+                    className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                  >
+                    Continuar
+                    <ArrowRight className="size-5 text-lime-950" />
+                  </button>
+                </div>
+              )
+              // : (
+              //   "fechado"
+              // aqui pode se usar isto ou null )
+            }
           </div>
           <p className="text-sm text-zinc-500">
             Ao planeja a sua viagem pela plann.er você automaticamente concorda{" "}
