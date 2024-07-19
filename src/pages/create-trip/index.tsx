331661e2af84bf07810fba233f2fedf94c2f0e2c
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-07-19 15:42:22 +0100

    tentando resolver erro

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,4 +1,3 @@
-import { ArrowRight, UserRoundPlus } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { InviteGuestModal } from "./invite-guests-modal";
@@ -80,41 +79,7 @@ export function CreateTripPage() {
               OpenIsGuestInput={OpenIsGuestInput}
               isGuestsInputOpen={isGuestsInputOpen}
             />
-            {/* //aqui pode se colocar o ponto de interrogação ? também  */}
-            {
-              isGuestsInputOpen && (
-                <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
-                  <button
-                    type="button"
-                    onClick={OpenIsGuestModal}
-                    className="flex items-center gap-2  flex-1"
-                  >
-                    <UserRoundPlus className="size-5 text-zinc-400" />
-                    {emailsToInvites.length > 0 ? (
-                      <span className="text-zinc-100 text-lg text-left">
-                        {emailsToInvites.length} pessoa(s) convidada(s)
-                      </span>
-                    ) : (
-                      <span className="text-zinc-400 text-lg text-left">
-                        Quem estará na viagem?
-                      </span>
-                    )}
-                  </button>
-
-                  <div className="w-px h-6 bg-zinc-800"></div>
-                  <button
-                    className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-                    onClick={OpenIsConfirmTripModal}
-                  >
-                    Confirmar viagem
-                    <ArrowRight className="size-5 text-lime-950" />
-                  </button>
-                </div>
-              )
-              // : (
-              //   "fechado"
-              // aqui pode se usar isto ou null )
-            }
+            {isGuestsInputOpen && ()}
           </div>
           <p className="text-sm text-zinc-500">
             Ao planeja a sua viagem pela plann.er você automaticamente concorda{" "}
