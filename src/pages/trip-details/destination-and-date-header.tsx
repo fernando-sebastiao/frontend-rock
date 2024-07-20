bc7b11fbee3aba8275d4b8e4f1b48714345b9b39
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-07-20 18:40:43 +0100

    tirado o bug e trazendo assim os dados do banco de dados como convidado

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,8 +1,8 @@
 import { MapPin, Calendar, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
 import { useParams } from "react-router-dom";
-import { api } from "../../lib/axios";
 import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
 import { format } from "date-fns";
 
 interface Trip {
@@ -16,33 +16,35 @@ interface Trip {
 export function DestinationAndDateHeader() {
   const { tripId } = useParams();
   const [trip, setTrip] = useState<Trip | undefined>();
+
   useEffect(() => {
-    api.get(`/trips/${tripId}`).then((response) => setTrip(response.data.trip));
+    api.get(`trips/${tripId}`).then((response) => setTrip(response.data.trip));
   }, [tripId]);
 
-  const displayedDate =
-    trip && trip.starts_at && trip.ends_at
-      ? format(trip?.starts_at, "d' de 'LLL")
-          .concat(" até ")
-          .concat(format(trip.ends_at, "d' de 'LLL"))
-      : null;
+  const displayedDate = trip
+    ? format(trip.starts_at, "d' de 'LLL")
+        .concat(" até ")
+        .concat(format(trip.ends_at, "d' de 'LLL"))
+    : null;
 
   return (
-    <div className="px-16 h-16 rounded-l bg-zinc-900 flex items-center justify-between">
+    <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
       <div className="flex items-center gap-2">
         <MapPin className="size-5 text-zinc-400" />
-        <span className=" text-zinc-100">{trip?.destination}</span>
+        <span className="text-zinc-100">{trip?.destination}</span>
       </div>
 
       <div className="flex items-center gap-5">
         <div className="flex items-center gap-2">
           <Calendar className="size-5 text-zinc-400" />
-          <span className=" text-zinc-100">{displayedDate}</span>
+          <span className="text-zinc-100">{displayedDate}</span>
         </div>
+
         <div className="w-px h-6 bg-zinc-800" />
-        <Button variant="secundary" size="default">
+
+        <Button variant="secundary">
           Alterar local/data
-          <Settings2 className="size-5 text-zinc-200" />
+          <Settings2 className="size-5" />
         </Button>
       </div>
     </div>
--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,37 +1,57 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button } from "../../components/button";
+import { api } from "../../lib/axios";
+
+interface Participant {
+  id: string;
+  name: string | null;
+  email: string;
+  is_confirmed: boolean;
+}
 
 export function Guests() {
+  const { tripId } = useParams();
+  const [participants, setParticipants] = useState<Participant[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`trips/${tripId}/participants`)
+      .then((response) => setParticipants(response.data.participants));
+  }, [tripId]);
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
+
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5 flex-1">
-            <span className="block font-medium text-zinc-100">
-              Jessica White
-            </span>
-            <span className="block text-sm text-zinc-400 truncate">
-              jessica.white44.yahoo.com
-            </span>
+        {participants.map((participant, index) => (
+          <div
+            key={participant.id}
+            className="flex items-center justify-between gap-4"
+          >
+            <div className="space-y-1.5">
+              <span className="block font-medium text-zinc-100">
+                {participant.name ?? `Convidado ${index}`}
+              </span>
+              <span className="block text-sm text-zinc-400 truncate">
+                {participant.email}
+              </span>
+            </div>
+
+            {participant.is_confirmed ? (
+              <CheckCircle2 className="text-green-400 size-5 shrink-0" />
+            ) : (
+              <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+            )}
           </div>
-          <CircleDashed className="size-5 text-zinc-400" />
-        </div>
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5 flex-1">
-            <span className="block font-medium text-zinc-100">
-              Dr, Rita Pachoca
-            </span>
-            <span className="block text-sm text-zinc-400 truncate">
-              lecy.styleomai@gmail.com
-            </span>
-          </div>
-          <CircleDashed className="size-5 text-zinc-400" />
-        </div>
+        ))}
       </div>
+
       <Button variant="secundary" size="full">
-        <UserCog className="size-5 text-zinc-200" />
-        Cadastrar novo link
+        <UserCog className="size-5" />
+        Gerenciar convidados
       </Button>
     </div>
   );
