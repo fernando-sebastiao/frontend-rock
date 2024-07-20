147c2c26ffab6222736eafb77a83915b1f0b33a7
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-07-20 23:26:45 +0100

    cadastrando actividades

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,38 +1,67 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { CircleCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface Activity {
+  date: string;
+  activities: [
+    {
+      id: string;
+      title: string;
+      accours_at: string;
+    }
+  ];
+}
 
 export function Activities() {
+  const { tripId } = useParams();
+  const [activites, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data.activities));
+  }, [tripId]);
   return (
     <div className="space-y-8">
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-          <span className="text-xs text-zinc-500">Sabado</span>
-        </div>
-        <p className="text-zinc-500 text-sm">
-          Nenhuma atividade cadastrada nesta data
-        </p>
-      </div>
-
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 19</span>
-          <span className="text-xs text-zinc-500">Domingo</span>
-        </div>
-        <div className="space-y-2.5">
-          <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-sm flex items-center gap-3">
-            <CircleCheck className="size-5 text-lime-300" />
-            <span className="text-zinc-100">Academia em grupo</span>
-            <span className="text-zinc-100 text-sm ml-auto">08:00h</span>
+      {activites.map((category) => {
+        return (
+          <div key={category.date} className="space-y-2.5">
+            <div className="flex gap-2 items-baseline">
+              <span className="text-xl text-zinc-300 font-semibold">
+                Dia {format(category.date, "d")}
+              </span>
+              <span className="text-xs text-zinc-500">
+                {format(category.date, "EEEE", { locale: ptBR })}
+              </span>
+            </div>
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map((activity) => {
+                  return (
+                    <div key={activity.id} className="space-y-2.5">
+                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-sm flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="text-zinc-100 text-sm ml-auto">
+                          {format(activity.accours_at, "hh:mm")}h
+                        </span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <p className="text-zinc-500 text-sm">
+                Nenhuma atividade cadastrada nesta data
+              </p>
+            )}
           </div>
-        </div>
-        <div className="space-y-2.5">
-          <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-sm flex items-center gap-3">
-            <CircleCheck className="size-5 text-lime-300" />
-            <span className="text-zinc-100">Academia em grupo</span>
-            <span className="text-zinc-100 text-sm ml-auto">08:00h</span>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
